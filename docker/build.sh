3943cdaba03cb974627b16eabc6fc3466751fb22
Ubuntu <abhilashnitk3@gmail.com>
2021-01-26 17:54:15 +0000

    OneContainer-One verticle
    Implemented by defining docker stack file.
    Instructions to deploy:
    1) docker login dockerhub.iudx.io in swarm manager node
    2) to bring up the stack, from swarm manager:  docker stack deploy --with-registry-auth -c rs-stack.yml rs

--- docker/build.sh
+++ docker/build.sh
@@ -3,3 +3,4 @@
 # To be executed from project root
 docker build -t iudx/rs-depl:latest -f docker/depl.dockerfile .
 docker build -t iudx/rs-dev:latest -f docker/dev.dockerfile .
+docker build -t dockerhub.iudx.io/iudx/rs:latest -f docker/depl.dockerfile . && docker push dockerhub.iudx.io/iudx/rs:latest 
